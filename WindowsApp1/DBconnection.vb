afbd88b94df7b90649d6e5854acee2bd47a6caf1
Arindam Maji <arindam9088.maji@gmail.com>
2022-10-19 21:19:59 +0530

    initial commit

---
+++ WindowsApp1/DBconnection.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Security.Cryptography
+Imports MySql.Data.MySqlClient
+Public Class DBconnection
+    Dim con As New MySqlConnection("Server=localhost; user=root; password=;database=mmj")
+    Dim cmd As New MySqlCommand
+    Dim da As New MySqlDataAdapter
+    Dim result As String
+
+    Public Function open() As MySqlConnection
+        Try
+            con.Open()
+        Catch ex As Exception
+
+        End Try
+        Return con
+    End Function
+
+    Public Function close() As MySqlConnection
+        Try
+            con.Close()
+        Catch ex As Exception
+
+        End Try
+        Return con
+    End Function
+
+    Public Sub create(ByVal sql As String)
+        Try
+            con.Open()
+            With cmd
+                .Connection = con
+                result = cmd.ExecuteNonQuery
+                If result = 0 Then
+                    MessageBox.Show("Failed to Save Data")
+                End If
+            End With
+        Catch ex As Exception
+        Finally
+            con.Close()
+        End Try
+    End Sub
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports MySql.Data.MySqlClient
+Imports System.Drawing.Printing
+Imports System.Security.Cryptography
+
+Public Class Form1
+#Region "Declares"
+    Dim myconnection As New DBconnection
+    Dim connadapter As MySqlDataAdapter
+    Dim datatable As DataTable
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        ComboBox1.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
+
+    End Sub
+
+    Public Function GetConnadapter() As MySqlDataAdapter
+        Return connadapter
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        connadapter = New MySqlDataAdapter("Select * From login where Uname = '" & TextBox1.Text & "' And Password = '" & TextBox2.Text & "' And Type = '" & ComboBox1.Text & "'", myconnection.open)
+        datatable = New DataTable
+        datatable.Clear()
+        connadapter.Fill(datatable)
+        If datatable.Rows.Count() <= 0 Then
+            MessageBox.Show("No One Found")
+        Else
+            If ComboBox1.Text = "Admin" Then
+                MessageBox.Show("Welcome " & TextBox1.Text & "")
+                Me.Hide()
+                Form2.Show()
+            End If
+            If ComboBox1.Text = "User" Then
+                MessageBox.Show("Welcome " & TextBox1.Text & "")
+                Me.Hide()
+                Form3.Show()
+
+            End If
+        End If
+    End Sub
+
+
+
+
+#End Region
+End Class
